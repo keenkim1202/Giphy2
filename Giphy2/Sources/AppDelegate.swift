e9fb4e36bf59c3f6b7f9fb2150c2ced4a7edcfb7
keen <homurahomus@gmail.com>
2021-01-10 17:10:31 +0900

    make FeedViewController

--- Giphy2/Sources/AppDelegate.swift
+++ Giphy2/Sources/AppDelegate.swift
@@ -16,23 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     let rect = UIScreen.main.bounds
     let window = UIWindow(frame: CGRect(x: 0, y: 0, width: rect.width, height: rect.height))
-    window.rootViewController = ViewController()
-    
+    window.rootViewController = FeedViewController()
     window.makeKeyAndVisible()
     
     self.window = window
     return true
   }
-
-  // MARK: UISceneSession Lifecycle
-
-  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-  }
-
-  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-  }
-
 
 }
 
---
+++ Giphy2/Sources/models/UserType.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Giphy2
+//
+//  Created by Heyziin on 2021/01/10.
+//
+
+import Foundation
+
+struct UserType {
+  var name: String
+  var address: String
+  var thumbnailUrl: String
+}
