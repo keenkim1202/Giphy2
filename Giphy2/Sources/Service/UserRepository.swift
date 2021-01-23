5f68102bad3572ff3035140d14739014eaed8e32
KEEN <homurahomus@gmail.com>
2021-01-23 16:12:53 +0900

    userRepository 구현

---
+++ Giphy2/Sources/Service/UserRepository.swift
@@ -0,0 +1,40 @@
+//
+//  UserRepository.swift
+//  Giphy2
+//
+//  Created by KEEN on 2021/01/23.
+//
+
+import Foundation
+
+protocol UserRepositoryType {
+    var count: Int { get }
+    func user(userRow: Int) -> UserType?
+    func add(userType: UserType)
+    func fetch() -> [UserType]
+}
+
+class UserRepository: UserRepositoryType {
+    private var items: [UserType]
+    
+    init() {
+        items = []
+    }
+    
+    var count: Int {
+        return items.count
+    }
+
+    func add(userType: UserType) {
+        items.append(userType)
+    }
+    
+    func fetch() -> [UserType] {
+        return []
+    }
+    
+    func user(userRow: Int) -> UserType? {
+        guard userRow < count else { return nil }
+        return items[userRow]
+    }
+}
