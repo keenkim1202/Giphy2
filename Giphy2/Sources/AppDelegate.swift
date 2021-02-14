a7149f56e931d723adba086bb6d188ecc435dd71
KEEN <homurahomu@naver.com>
2021-02-14 15:04:36 +0900

    Giphy APIKey 설정

--- Giphy2/Sources/AppDelegate.swift
+++ Giphy2/Sources/AppDelegate.swift
@@ -19,9 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let rect = UIScreen.main.bounds
     let window = UIWindow(frame: CGRect(x: 0, y: 0, width: rect.width, height: rect.height))
     let userRepository = UserRepository()
-        
-    loadData(repo: userRepository)
-    
     let rootVC = FeedViewController(userRepository: userRepository)
     let nvc = UINavigationController(rootViewController: rootVC)
     window.rootViewController = nvc
@@ -32,10 +29,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     self.userRepository = userRepository
     return true
   }
-  
-  private func loadData(repo: UserRepositoryType) {
-    repo.add(userType: UserType(name: "kim", address: "서울", thumbnailUrl: "https://image.ytn.co.kr/general/jpg/2020/1207/202012071325561445_t.jpg"))
-  }
 
+  private func loadAPIKey() -> String? {
+    guard let path = Bundle.main.path(forResource: "Giphy", ofType: "plist") else { return nil }
+    guard let data = try? Data(contentsOf: URL(fileURLWithPath: path)) else { return nil }
+    guard let plist = try? PropertyListSerialization.propertyList(from: data, options: .mutableContainers, format: .none) as? [String:String] else { return nil }
+    guard let apiKey = plist["APIKEY"] else { return nil }
+    
+    return apiKey
+  }
 }
 
---
+++ Giphy2/Sources/Service/GiphyApiService.swift
@@ -0,0 +1,25 @@
+//
+//  GiphyApiService.swift
+//  Giphy2
+//
+//  Created by KEEN on 2021/02/14.
+//
+
+import Foundation
+
+protocol GiphyApiServiceType {
+  func fetch()
+}
+
+final class GiphyApiService: GiphyApiServiceType {
+  
+  init() {
+    
+  }
+  
+  func fetch() {
+    
+  }
+  
+  
+}
